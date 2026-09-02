70e82aa7b66a36ad66b08896b492f65ece593f6a
Ravishani <ravi@s2code.com>
2026-09-02 18:22:19 +0530

    Update backend

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@ const nextConfig = {
     return [
       {
         source: "/admin/login",
-        destination: "https://api.globaltechnext.com/admin/login",
+        destination: "http://127.0.0.1:8000/admin/login",
         permanent: false,
       },
     ];
